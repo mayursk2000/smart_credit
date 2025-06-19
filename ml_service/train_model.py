94e9f0ca03e696e26327ec3d0bec0fbcb2a79ebe
Mayur <mayursk2000@gmail.com>
2025-06-19 20:30:04 +0530

    model_logic

---
+++ ml_service/train_model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import xgboost as xgb
+import shap
+import joblib
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+
+
+
